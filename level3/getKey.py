b64763ba043acd908bd9d0cc5bd87e8b4ae5ae5f
jason3e7 <jason3e7@gmail.com>
2015-12-08 16:22:28 +0800

    Remove level3 password

--- level3/getKey.py
+++ level3/getKey.py
@@ -4,7 +4,7 @@ import requests
 import re
 
 username = 'natas3'
-password = 'sJIJNW6ucpu6HPZ1ZAchaDtwd7oGrD14'
+password = ''
 
 r = requests.get('http://natas3.natas.labs.overthewire.org/s3cr3t/users.txt', auth=(username, password))
 pattern = r'natas4:(\w+)'
